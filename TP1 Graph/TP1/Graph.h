4c80db51fe2ce3f380cb85e99442ffee4c1285c4
Machine <abderrahim223@gmail.com>
2019-10-30 16:47:26 -0400

    implement getVoisin() Sommet AA

--- TP1 Graph/TP1/Graph.h
+++ TP1 Graph/TP1/Graph.h
@@ -30,15 +30,15 @@ class Graph
 	void creerGraphe(const string& nomFichier);
 
 	void afficherGraphe(vector<Sommet> graphe);
-	void prendreCommande();
-	void afficherCommande();
+	//void prendreCommande();	// A faire
+	//void afficherCommande();  // A faire
 	Sommet getSommet(int numero); 
 	//Sommet* getReferenceSommet(int id);
 	//void updateSommetObjects(vector<int> objects);
 
 	private:
 	Sommet* trouverSommet(const string& numero);
-	//void viderGraphe();
+	//void viderGraphe(); // A faire
 	//int findNext(Sommet noeud);
 	std::vector<Sommet *> tabSommets_;
 	std::vector<Arc*> tabArcs_;
