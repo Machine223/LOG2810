626e67520d0270a38365cdb1da4173c3e6ea438b
Hanane ikhelef <hananeikhelef@gmail.com>
2019-10-30 15:15:03 -0400

    setter/getter

--- TP1 Graph/TP1/Sommet.cpp
+++ TP1 Graph/TP1/Sommet.cpp
@@ -2,10 +2,15 @@
 
 
 
-Sommet::Sommet()
-{
-}
 
+Sommet::Sommet(int numero, int nbObjetA, int nbObjetB, int nbObjetC)
+{
+	this->numero_ = numero;
+	this->nbObjetA_ = nbObjetA;
+	this->nbObjetB_ = nbObjetB;
+	this->nbObjetC_ = nbObjetC;
+
+}
 
 Sommet::~Sommet()
 {
